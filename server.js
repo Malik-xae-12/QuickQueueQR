2dc231ba9d1684a46ace7c53b45dd7f50778f83a
Malik-xae-12 <141534004+Malik-xae-12@users.noreply.github.com>
2024-04-02 19:05:11 +0530

    Update server.js

--- server.js
+++ server.js
@@ -17,8 +17,8 @@ dotenv.config();
 connectDB();
 
 //es module fix
-const __filename= fileURLToPath(import.meta.url);
-const __dirname=path.dirname(__filename);
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 //rest object
 const app = express();
@@ -27,8 +27,7 @@ const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(cors());
-app.use(express.static(path.join(__dirname,'./client/build')));
-
+app.use(express.static(path.join(__dirname, "./client/build")));
 
 //routes
 app.use("/api/v1/auth", authRoute);
@@ -36,10 +35,9 @@ app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
 //rest api
-app.use('*',function(req,res){
-  res.sendFile(path.join(__dirname,"./client/build/index.html"))
-})
-
+app.use("*", function (req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 //port
 const PORT = process.env.PORT || 8080;
